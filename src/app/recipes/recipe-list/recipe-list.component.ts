24fea775bb1af850683a3dd5207526c753b68645
ChamodKavinda <kavindachamod100@gmail.com>
2024-05-03 15:18:48 +0530

    create nav bar

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','simple test','https://media.istockphoto.com/id/1599002141/photo/woman-serving-two-bowls-of-pumpkin-soup-for-thanksgiving-meal.jpg?s=1024x1024&w=is&k=20&c=KXQ-z_CjgdJO9n46plLfDbdudn-uhMcCiO4fIAjPLTQ=')
+    ];
 
 }
